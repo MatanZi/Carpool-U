31b6f182d2ab5381fcb34f311701ae823de8e4ee
MatanZ <matanzilka@gmail.com>
2018-12-24 17:00:37 +0200

    Create Admin.java

---
+++ implementation_phase/carpool_2/app/src/main/java/johannt/carpool_2/Admin.java
@@ -0,0 +1,18 @@
+package johannt.carpool_2;
+
+public class Admin extends User {
+    private boolean isAdmin;
+
+    public Admin(String firstName, String lastName, String email, String password, String phoneNumber, String city, String university, String id, boolean isAdmin) {
+        super(firstName, lastName, email, password, phoneNumber, city, university, id);
+        this.isAdmin = isAdmin;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+}
