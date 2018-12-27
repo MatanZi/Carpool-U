8f3290a65121ee0ac6334b0b327afc6ab52fb66b
MatanZ <matanzilka@gmail.com>
2018-12-27 13:11:00 +0200

    update findride and create result activity

---
+++ implementation_phase/carpool_2/app/src/main/java/johannt/carpool_2/Rides_And_Validator/ResultActivity.java
@@ -0,0 +1,38 @@
+package johannt.carpool_2.Rides_And_Validator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+
+import johannt.carpool_2.R;
+
+public class ResultActivity extends AppCompatActivity {
+
+    private ArrayList<Carpool> carpoolList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+        carpoolList = (ArrayList<Carpool>)intent.getSerializableExtra("carpoolList");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
