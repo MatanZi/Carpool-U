b6175ca54d0944166c2182e318ce74a879c5d484
MatanZ <matanzilka@gmail.com>
2018-12-30 23:06:49 +0200

    update gradile

--- implementation_phase/carpool_2/app/src/main/java/johannt/carpool_2/Profile_Features/FindRideActivity.java
+++ implementation_phase/carpool_2/app/src/main/java/johannt/carpool_2/Profile_Features/FindRideActivity.java
@@ -185,17 +185,12 @@ public class FindRideActivity extends AppCompatActivity implements View.OnClickL
 
             validator = new Validator();
 
-            try {
-                checker = validator.checkDate(date, this) &&
-                        validator.checkTime(endTime, this) &&
-                        validator.checkTime(startTime, this)&&
-                        validator.checkPrice(price, this) &&
-                        validator.checkdst(dst, this) &&
-                        validator.checkSrc(src, this) ;
-
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
+            checker = validator.checkDate(date, this) &&
+                    validator.checkTime(endTime, this) &&
+                    validator.checkTime(startTime, this)&&
+                    validator.checkPrice(price, this) &&
+                    validator.checkdst(dst, this) &&
+                    validator.checkSrc(src, this) ;
 
 
             if (checker) {
