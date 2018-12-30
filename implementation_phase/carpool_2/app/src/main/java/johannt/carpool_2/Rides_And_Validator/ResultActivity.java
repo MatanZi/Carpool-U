6d0f3284a3c74e48b720ba340c9bf95d74beaf8b
johant93 <thuillierjohann@gmail.com>
2018-12-30 21:16:46 +0200

    SMS,Whatsapp,call done

--- implementation_phase/carpool_2/app/src/main/java/johannt/carpool_2/Rides_And_Validator/ResultActivity.java
+++ implementation_phase/carpool_2/app/src/main/java/johannt/carpool_2/Rides_And_Validator/ResultActivity.java
@@ -1,10 +1,14 @@
 package johannt.carpool_2.Rides_And_Validator;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
@@ -35,6 +39,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import johannt.carpool_2.Profile_Features.FindRideActivity;
+import johannt.carpool_2.Profile_Features.ProfileActivity;
 import johannt.carpool_2.R;
 import johannt.carpool_2.Users.User;
 
@@ -55,11 +60,6 @@ public class ResultActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private DatabaseReference firebaseDatabaseRides,firebaseUsersRef,userRef;
     private FirebaseDatabase databaseCarPool;
-<<<<<<< HEAD
-=======
-    private FirebaseUser firebaseUser;
-    private String userID;
->>>>>>> c5e7b72103358c2a6359465f2da3b9ddec4039ae
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,16 +76,13 @@ public class ResultActivity extends AppCompatActivity {
         //firebaseUser = firebaseAuth.getCurrentUser();
         databaseCarPool = FirebaseDatabase.getInstance();
         firebaseDatabaseRides = databaseCarPool.getReference("Rides");
-<<<<<<< HEAD
         firebaseUsersRef = databaseCarPool.getReference("Users");
-=======
-        userID = firebaseUser.getUid();
->>>>>>> c5e7b72103358c2a6359465f2da3b9ddec4039ae
 
         carpoolListView = findViewById(R.id.list_view_carpool);
 
 
         carpoolList = new ArrayList<>();
+
 
         Intent intent = getIntent();
         date = intent.getStringExtra("date");
@@ -98,7 +95,6 @@ public class ResultActivity extends AppCompatActivity {
         ride = new Carpool();
         driver = new User();
         validator = new Validator();
-<<<<<<< HEAD
 
 
         carpoolListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -129,13 +125,14 @@ public class ResultActivity extends AppCompatActivity {
         });
         return  driver ;
     }
-=======
->>>>>>> c5e7b72103358c2a6359465f2da3b9ddec4039ae
 
+    @Override
+    protected void onStart() {
+        super.onStart();
         carpoolList.clear();
         firebaseDatabaseRides.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot rideSnapshot : dataSnapshot.getChildren()) {
                     ride = rideSnapshot.getValue(Carpool.class);
                     checkDates = date.equals(ride.getDate()) &&
@@ -166,7 +163,6 @@ public class ResultActivity extends AppCompatActivity {
             }
         });
     }
-<<<<<<< HEAD
 
     private void showContactDialog(final Carpool carpool,final User actualDriver,String Name) {
 
@@ -177,7 +173,7 @@ public class ResultActivity extends AppCompatActivity {
 
         final TextView fullName = (TextView) dialogView.findViewById(R.id.fullName);
         final TextView numOfFreeplace = (TextView) dialogView.findViewById(R.id.numOfFreePlace);
-      //  final ImageView imageProfile = (ImageView) dialogView.findViewById(R.id.profilePict);
+        //  final ImageView imageProfile = (ImageView) dialogView.findViewById(R.id.profilePict);
         final ImageButton buttonSms = (ImageButton) dialogView.findViewById(R.id.smsButton);
         final ImageButton buttonCall = (ImageButton) dialogView.findViewById(R.id.callButton);
         final ImageButton buttonWhatsapp = (ImageButton) dialogView.findViewById(R.id.whatsappButton);
@@ -194,7 +190,17 @@ public class ResultActivity extends AppCompatActivity {
         buttonCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String numberToCall = "tel:"+driver.getPhoneNumber();
                 Toast.makeText(getApplicationContext(), "Call", Toast.LENGTH_SHORT).show();
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse(numberToCall));
+
+                if (ActivityCompat.checkSelfPermission(ResultActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                startActivity(callIntent);
                 b.dismiss();
 
             }
@@ -206,6 +212,11 @@ public class ResultActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "SMS", Toast.LENGTH_SHORT).show();
 
+                String content = "Carpool-U:\nHey,\ncan you reserve me a sit for the following drive:\n"+
+                        carpool.getStartTime()+"-"+carpool.getEndTime()+"  "+carpool.getDate()
+                        +"\n?\nThanks, "+ ProfileActivity.firstName;
+                sendSMS(driver.getPhoneNumber(),content);
+
                 b.dismiss();
 
             }
@@ -214,6 +225,13 @@ public class ResultActivity extends AppCompatActivity {
         buttonWhatsapp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "a free place for "+carpool.getDate()+" ?");
+                sendIntent.setType("text/plain");
+                sendIntent.setPackage("com.whatsapp");
+                startActivity(sendIntent);
                 Toast.makeText(getApplicationContext(), "WhatsApp", Toast.LENGTH_SHORT).show();
                 b.dismiss();
 
@@ -221,7 +239,24 @@ public class ResultActivity extends AppCompatActivity {
         });
     }
 
+    protected void sendSMS(String number,String content) {
+        Log.i("Send SMS", "");
+        Intent smsIntent = new Intent(Intent.ACTION_VIEW);
 
-=======
->>>>>>> c5e7b72103358c2a6359465f2da3b9ddec4039ae
+        smsIntent.setData(Uri.parse("smsto:"+number));
+//        smsIntent.setType("vnd.android-dir/mms-sms");
+//        smsIntent.putExtra("address"  , new String ("01234"));
+          smsIntent.putExtra("sms_body"  , content);
+
+        try {
+            startActivity(smsIntent);
+            finish();
+            Log.i("Finished sending SMS...", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(ResultActivity.this,
+                    "SMS faild, please try again later.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
