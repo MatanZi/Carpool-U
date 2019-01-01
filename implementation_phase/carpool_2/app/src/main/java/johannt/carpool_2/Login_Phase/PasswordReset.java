ccdb57b52d3fd714c77508eab3918e5a6a413615
MatanZ <matanzilka@gmail.com>
2019-01-01 19:57:22 +0200

    update

--- implementation_phase/carpool_2/app/src/main/java/johannt/carpool_2/Login_Phase/PasswordReset.java
+++ implementation_phase/carpool_2/app/src/main/java/johannt/carpool_2/Login_Phase/PasswordReset.java
@@ -1,30 +1,44 @@
 package johannt.carpool_2.Login_Phase;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import johannt.carpool_2.R;
+import johannt.carpool_2.Rides_And_Validator.Validator;
+import johannt.carpool_2.Users.User;
 
 public class PasswordReset extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText editTextEmail;
-    private Button PasswordResetBtn;
+    private EditText editTextEmail ;
+    private Button PasswordResetButton;
     private String email;
+    private Validator validator;
+    private User secondUser;
+    private ProgressDialog progressDialog;
 
-    //firebase auth object
+    //firebase object
     private FirebaseAuth firebaseAuth;
-    private FirebaseUser user;
+    private DatabaseReference firebaseDatabaseRides;
+    private DatabaseReference firebaseDatabaseUsers;
+    private FirebaseDatabase databaseCarPool;
+    private FirebaseUser firebaseUser;
+    private Boolean flag = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,56 +46,58 @@ public class PasswordReset extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_password_reset);
 
         editTextEmail = findViewById(R.id.editTextEmail);
-        user = firebaseAuth.getCurrentUser();
+        validator = new Validator();
 
-        email = editTextEmail.getText().toString().trim();
-        PasswordResetBtn = findViewById(R.id.PasswordResetBtn);
-        PasswordResetBtn.setOnClickListener(this);
+        PasswordResetButton = findViewById(R.id.PasswordResetBtn);
+        PasswordResetButton.setOnClickListener(this);
+        progressDialog = new ProgressDialog(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        databaseCarPool = FirebaseDatabase.getInstance();
+        firebaseDatabaseUsers = databaseCarPool.getReference("Users");
+
+
+
     }
-
-
-    private void passwordReset(){
-            FirebaseAuth.getInstance().sendPasswordResetEmail(firebaseAuth.getCurrentUser().getEmail())
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                Toast toast = Toast.makeText(PasswordReset.this,"Mail sent , please check your email inbox", Toast.LENGTH_SHORT);
-                                toast.setGravity(Gravity.TOP, 0, 0);
-                                toast.show();
-                                return;
-                            }
-                        }
-                    });
-
-/**            user.sendEmailVerification()
-                    .addOnCompleteListener(this, new OnCompleteListener() {
-                        @Override
-                        public void onComplete(@NonNull Task task) {
-                            // Re-enable button
-                            findViewById(R.id.verify_email_button).setEnabled(true);
-
-                            if (task.isSuccessful()) {
-                                Toast.makeText(EmailPasswordActivity.this,
-                                        "Verification email sent to " + user.getEmail(),
-                                        Toast.LENGTH_SHORT).show();
-                            } else {
-                                Log.e("STATE", "sendEmailVerification", task.getException());
-                                Toast.makeText(EmailPasswordActivity.this,
-                                        "Failed to send verification email.",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });**/
-        }
-           // Toast.makeText(PasswordReset.this,"Email not found", Toast.LENGTH_SHORT).show();
-
-
 
     @Override
-    public void onClick(View view) {
-        if (view == PasswordResetBtn) {
-            passwordReset();
+    public void onClick(View v) {
+        email = editTextEmail.getText().toString().trim();
+        if(validator.checkEmail(email, this)){
+            progressDialog.setMessage("Loading Please Wait...");
+            progressDialog.show();
+            PasswordRecovery(email);
         }
     }
+
+
+    private void PasswordRecovery(final String email){
+        firebaseDatabaseUsers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot userSnapshot : dataSnapshot.getChildren()){
+                    secondUser = userSnapshot.getValue(User.class);
+                    if(email.equals(secondUser.getEmail())){
+                        progressDialog.dismiss();
+                        flag = true;
+                        Toast.makeText(PasswordReset.this, "Your password is: "+ secondUser.getPassword() , Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                }
+                if(!flag){
+                    progressDialog.dismiss();
+                    Toast.makeText(PasswordReset.this, "Email doesnt exist" , Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Failed to read value
+                Log.w("Failed to read value.", databaseError.toException());
+
+            }
+        });
+    }
+
 }
